bea491ccaa64e8a6a6f03b72aafcc093572d0442
whale <lje1203@yonsei.ac.kr>
2023-08-10 17:34:49 +0900

    [08.10] exercise_direction1 edit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import styled from 'styled-components';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Exercise_road_3 from './practice2/exercise_road3';
-import Exercise_road_4 from './practice2/exercise_road4';
-import Exercise_road_0 from './practice2/exercise_road0';
-import Exercise_road_2 from './practice2/exercise_road2';
+// import Exercise_road_3 from './practice2/excercise_road3';
+// import Exercise_road_4 from './practice2/excercise_road4';
+// import Exercise_road_0 from './practice2/excercise_road0';
+// import Exercise_road_2 from './practice2/excercise_road2';
+import Exercise_direction1 from './practice1/exercise_direction1';
+
 // import Tut2 from './tutorial/tut2';
 // import Maphome from './maphome';
 // import MapHomeBox from './MapHomeBox';
@@ -29,13 +31,15 @@ const Container = styled.div`
 function App() {
   return (
     <Container>
-      <Router>
+      <Exercise_direction1 />
+      {/* <Router>
         <Switch>
-          <Route exact path="/"><Exercise_road_2/></Route> 
+          <Route exact path="/"><Exercise_road_0/></Route> 
+          <Route exact path="/exercise_road_2"><Exercise_road_2/></Route> 
           <Route exact path="/exercise_road_3"><Exercise_road_3/></Route> 
           <Route exact path="/exercise_road_4"><Exercise_road_4/></Route> 
         </Switch>
-      </Router>
+      </Router> */}
     </Container>
   );
 }
