89281a3f3d9e036abb3a4d49d2d4985ab6ea2947
woojae <wjpandadavid@naver.com>
2023-08-12 01:29:34 +0900

    0812 01:29 우제 - 경로표시

--- src/practice2/exercise_road4.js
+++ src/practice2/exercise_road4.js
@@ -94,22 +94,63 @@ const Exercise_road_4 = () => {
 
 
               
-              <Polyline 
-                path= {[
-                  new window.naver.maps.LatLng(286,56.102),
-                  new window.naver.maps.LatLng(743,364.518),
-                  new window.naver.maps.LatLng(245,71.836),
-                  new window.naver.maps.LatLng(106,47.697),
-                  new window.naver.maps.LatLng(173,56.617),
-                  new window.naver.maps.LatLng(90,40.498),
-                  new window.naver.maps.LatLng(83,42.685),
-                ]}
-                // clickable // 사용자 인터랙션을 받기 위해 clickable을 true로 설정합니다.
-                strokeColor={'#ff3344'}
-                strokeStyle={'solid'}
-                strokeOpacity={0.8}
-                strokeWeight={4}        
-              />
+                  <Polyline 
+                  clickable={true}
+                  strokeColor="blue"
+                  strokeStyle="solid"
+                  strokeOpacity={0.8}
+                  strokeWeight={3}  
+                  path= {[
+                    new window.naver.maps.LatLng(37.5603582,126.9367624), 
+                    new window.naver.maps.LatLng(37.559840,126.936910), 
+                    new window.naver.maps.LatLng(37.559730,126.936913), 
+                    new window.naver.maps.LatLng(37.559524,126.936922), 
+                    new window.naver.maps.LatLng(37.559322,126.936928), 
+                    new window.naver.maps.LatLng(37.559299,126.936928), 
+                    new window.naver.maps.LatLng(37.559098,126.936930), 
+                    new window.naver.maps.LatLng(37.558974,126.936929), 
+                    new window.naver.maps.LatLng(37.558883,126.936929), 
+                    new window.naver.maps.LatLng(37.558518,126.936926), 
+                    new window.naver.maps.LatLng(37.557844,126.936915), 
+                    new window.naver.maps.LatLng(37.557675,126.936912), 
+                    new window.naver.maps.LatLng(37.557366,126.936916), 
+                    new window.naver.maps.LatLng(37.557338,126.936917), 
+                    new window.naver.maps.LatLng(37.557261,126.936912), 
+                    new window.naver.maps.LatLng(37.556993,126.936896), 
+                    new window.naver.maps.LatLng(37.556590,126.936874), 
+                    new window.naver.maps.LatLng(37.556444,126.936868), 
+                    new window.naver.maps.LatLng(37.556379,126.936866), 
+                    new window.naver.maps.LatLng(37.556314,126.936865), 
+                    new window.naver.maps.LatLng(37.556064,126.936861), 
+                    new window.naver.maps.LatLng(37.555974,126.936859), 
+                    new window.naver.maps.LatLng(37.555624,126.936858), 
+                    new window.naver.maps.LatLng(37.555580,126.936804), 
+                    new window.naver.maps.LatLng(37.555517,126.936729), 
+                    new window.naver.maps.LatLng(37.555466,126.936643), 
+                    new window.naver.maps.LatLng(37.555391,126.936442), 
+                    new window.naver.maps.LatLng(37.555374,126.936111), 
+                    new window.naver.maps.LatLng(37.555395,126.936060), 
+                    new window.naver.maps.LatLng(37.555445,126.935936), 
+                    new window.naver.maps.LatLng(37.555590,126.935554), 
+                    new window.naver.maps.LatLng(37.555644,126.935413), 
+                    new window.naver.maps.LatLng(37.555735,126.935168), 
+                    new window.naver.maps.LatLng(37.555877,126.935236), 
+                    new window.naver.maps.LatLng(37.556199,126.935436), 
+                    new window.naver.maps.LatLng(37.556255,126.935474), 
+                    new window.naver.maps.LatLng(37.556500,126.935636), 
+                    new window.naver.maps.LatLng(37.556565,126.935699), 
+                    new window.naver.maps.LatLng(37.556632,126.935781), 
+                    new window.naver.maps.LatLng(37.556868,126.936165), 
+                    new window.naver.maps.LatLng(37.556942,126.936287), 
+                    new window.naver.maps.LatLng(37.556952,126.936153), 
+                    new window.naver.maps.LatLng(37.556959,126.935680), 
+                    new window.naver.maps.LatLng(37.556965,126.935316), 
+                    new window.naver.maps.LatLng(37.556972,126.934809), 
+                    new window.naver.maps.LatLng(37.556985,126.934545), 
+                    new window.naver.maps.LatLng(37.556648,126.935483)]}
+                  // clickable // 사용자 인터랙션을 받기 위해 clickable을 true로 설정합니다.
+                        
+                />
              
               {departureRef.current && (
                 <Marker 
